098fc27080c2723cd78551563649f2b5cbe5fe42
Jaap de Koning <jaap.de.koning@bigdatarepublic.nl>
2020-05-15 08:57:47 +0200

    Sensible defaults with overrides in secret.auto.tfvars

--- variables.tf
+++ variables.tf
@@ -23,14 +23,24 @@ variable "update-log" {}
 variable "update-email" {}
 variable "update-retry" {}
 variable "api-authorization-key" {}
-variable "workshop-collection-id" {}
-variable "workshop-start-map" {}
+variable "workshop-collection-id" {
+   default = "125499818"
+}
+variable "workshop-start-map" {
+   default = "125488374"
+}
 variable "maxplayers" {
    default = "18"
 }
 variable "tickrate" {
    default = "64"
 }
-variable "extraparams" {}
-variable "param-start" {}
-variable "param-update" {}
+variable "extraparams" {
+   default = "-nohltv +sv_pure 0 +game_type 0 +game_mode 0 +mapgroup mg_bomb +map de_dust2"
+}
+variable "param-start" {
+   default = "-nobreakpad -game csgo -console -usercon -secure -autoupdate -steam_dir $DIR_STEAMCMD -steamcmd_script $STEAM_RUNSCRIPT -maxplayers_override $MAXPLAYERS -tickrate $TICKRATE +hostport $PORT +net_public_adr $IP $EXTRAPARAMS"
+}
+variable "param-update" {
+   default = "+login $STEAM_LOGIN $STEAM_PASSWORD +force_install_dir $DIR_ROOT +app_update 740 validate +quit"
+}
